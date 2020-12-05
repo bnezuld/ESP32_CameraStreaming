bebce6b92232b9bbde3666b1b5ae07a6f2fcb33d
bnezuld <bnezuld@yahoo.com>
2020-12-05 10:30:53 -0600

    adding camera task
    added file from https://github.com/espressif/esp32-camera to inside the esp-idf/component folder.
    
    make sure configuration is correct as the pins will be diffrent for boards
    
    adding camera task as well.

--- main/main.c
+++ main/main.c
@@ -26,6 +26,57 @@
 #include "nvs.h"
 #include "driver/gpio.h"
 
+#include "esp_camera.h"
+
+//WROVER-KIT PIN Map
+#define CAM_PIN_PWDN    32 //power down is not used
+#define CAM_PIN_RESET   -1 //software reset will be performed
+#define CAM_PIN_XCLK    0 //21
+#define CAM_PIN_SIOD    26 //26
+#define CAM_PIN_SIOC    27 //27
+
+#define CAM_PIN_D7      35 //35
+#define CAM_PIN_D6      34 //34
+#define CAM_PIN_D5      39 //39
+#define CAM_PIN_D4      36 //36
+#define CAM_PIN_D3      21 //19
+#define CAM_PIN_D2      19 //18
+#define CAM_PIN_D1      18 //5
+#define CAM_PIN_D0       5 //4
+#define CAM_PIN_VSYNC   25 //25
+#define CAM_PIN_HREF    23 //23
+#define CAM_PIN_PCLK    22 //22
+
+static camera_config_t camera_config = {
+    .pin_pwdn  = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sscb_sda = CAM_PIN_SIOD,
+    .pin_sscb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    //XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG,//YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_UXGA,//QQVGA-QXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 12, //0-63 lower number means higher quality
+    .fb_count = 1 //if more than one, i2s runs in continuous mode. Use only with JPEG
+};
 
 #define STORAGE_NAMESPACE "storage"
 
@@ -43,6 +94,8 @@ static const char *TAG = "camera_streaming";
 
 
 static void smartconfig_example_task(void * parm);
+static void camera_task(void * param);
+
 
 esp_err_t SaveLoginInfo(uint8_t ssid[33], uint8_t password[65])
 {
@@ -216,6 +269,58 @@ static void smartconfig_example_task(void * parm)
     }
 }
 
+esp_err_t init_camera(){
+    //power up the camera if PWDN pin is defined
+    if(CAM_PIN_PWDN != -1){
+        //pinMode(CAM_PIN_PWDN, OUTPUT);
+        //digitalWrite(CAM_PIN_PWDN, LOW);
+    }
+
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera Init Failed");
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+esp_err_t camera_capture(){
+    //acquire a frame
+    camera_fb_t * fb = esp_camera_fb_get();
+    if (!fb) {
+        ESP_LOGE(TAG, "Camera Capture Failed");
+        return ESP_FAIL;
+    }
+    //replace this with your own function
+    //process_image(fb->width, fb->height, fb->format, fb->buf, fb->len);
+  
+    //return the frame buffer back to the driver for reuse
+    esp_camera_fb_return(fb);
+    return ESP_OK;
+}
+
+static void camera_task(void * param)
+{
+    esp_err_t err;
+    ESP_ERROR_CHECK(init_camera());
+    while(true)
+    {
+        err = camera_capture();
+        if(err != ESP_OK)
+        {
+            ESP_LOGE(TAG, "Camera Capture Failed");
+        }else
+        {
+            ESP_LOGI(TAG, "Camera Capture");
+        }
+        
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+}
+
 void app_main(void)
 {
     s_wifi_event_group = xEventGroupCreate();
@@ -254,4 +359,6 @@ void app_main(void)
             ESP_LOGI(TAG, "login succesfull");
         }
     }
+
+    xTaskCreate(camera_task, "camera_task", 4096, NULL, 3, NULL);
 }
