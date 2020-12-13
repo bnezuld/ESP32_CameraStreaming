2f84a53cf9da7a741b1d85897a8776838a163130
bnezuld <bnezuld@yahoo.com>
2020-12-12 18:38:35 -0600

    camera tasks
    camera task created to get images.
    
    and then a save task.
    
    using a mutex to safely access the camera data.

--- main/main.c
+++ main/main.c
@@ -30,12 +30,6 @@
 #include "esp_http_server.h"
 #include "esp_timer.h"
 
-#define PART_BOUNDARY "123456789000000000000987654321"
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
-
-
 //WROVER-KIT PIN Map
 #define CAM_PIN_PWDN    32 //power down is not used
 #define CAM_PIN_RESET   -1 //software reset will be performed
@@ -90,6 +84,7 @@ static camera_config_t camera_config = {
 
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
+static EventGroupHandle_t cameraTaskEventGroup;
 
 /* The event group allows multiple bits for each event, but we only care about two events:
  * - we are connected to the AP with an IP
@@ -100,8 +95,15 @@ static EventGroupHandle_t s_wifi_event_group;
 #define WIFI_SMARTCONFIG_DONE_BIT BIT3
 static const char *TAG = "camera_streaming";
 
+SemaphoreHandle_t cameraMutex = NULL;
+QueueHandle_t httpReqQueue = NULL;
+
+size_t _jpg_buf_len = 0;
+uint8_t * _jpg_buf = NULL;
 
 static void smartconfig_example_task(void * parm);
+static void CameraCapture_task(void * parm);
+static void SaveCamera_task(void * parm);
 
 esp_err_t SaveLoginInfo(uint8_t ssid[33], uint8_t password[65])
 {
@@ -294,81 +296,57 @@ esp_err_t init_camera(){
     return ESP_OK;
 }
 
-httpd_handle_t stream_httpd = NULL;
-
-static esp_err_t stream_handler(httpd_req_t *req){
-  camera_fb_t * fb = NULL;
+static void CameraCapture_task(void * parm)
+{
+  static camera_fb_t * fb = NULL;
   esp_err_t res = ESP_OK;
-  size_t _jpg_buf_len = 0;
-  uint8_t * _jpg_buf = NULL;
-  char * part_buf[64];
+  
+  while(true)
+  {  
+    if(xSemaphoreTake(cameraMutex, portMAX_DELAY))
+    {
+      if(fb){
+        esp_camera_fb_return(fb);
+        fb = NULL;
+        _jpg_buf = NULL;
+      } else if(_jpg_buf){
+        free(_jpg_buf);
+        _jpg_buf = NULL;
+      }
 
-  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-  if(res != ESP_OK){
-    return res;
-  }
-
-  while(true){
-    fb = esp_camera_fb_get();
-    if (!fb) {
-      ESP_LOGE(TAG, "Camera Capture Failed");
-      res = ESP_FAIL;
-    } else {
-      if(fb->width > 400){
-        if(fb->format != PIXFORMAT_JPEG){
-          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-          esp_camera_fb_return(fb);
-          fb = NULL;
-          if(!jpeg_converted){
-            ESP_LOGE(TAG, "JPEG compression failed");
-            res = ESP_FAIL;
+      fb = esp_camera_fb_get();
+      if (!fb) {
+        ESP_LOGE(TAG, "Camera Capture Failed");
+        res = ESP_FAIL;
+      } else {
+        if(fb->width > 400){
+          if(fb->format != PIXFORMAT_JPEG){
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            esp_camera_fb_return(fb);
+            fb = NULL;
+            if(!jpeg_converted){
+              ESP_LOGE(TAG, "JPEG compression failed");
+              res = ESP_FAIL;
+            }
+          } else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
           }
-        } else {
-          _jpg_buf_len = fb->len;
-          _jpg_buf = fb->buf;
         }
       }
+      xSemaphoreGive(cameraMutex);
     }
-    if(res == ESP_OK){
-      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-    }
-    if(res == ESP_OK){
-      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-    }
-    if(fb){
-      esp_camera_fb_return(fb);
-      fb = NULL;
-      _jpg_buf = NULL;
-    } else if(_jpg_buf){
-      free(_jpg_buf);
-      _jpg_buf = NULL;
-    }
-    if(res != ESP_OK){
-      break;
-    }
-    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
   }
-  return res;
 }
 
-void startCameraServer(){
-  httpd_config_t http_config = HTTPD_DEFAULT_CONFIG();
-  http_config.server_port = 80;
+static void SaveCamera_task(void * parm){
+  while(true)
+  {
+    if(xSemaphoreTake(cameraMutex, portMAX_DELAY))
+    {
 
-  httpd_uri_t index_uri = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = stream_handler,
-    .user_ctx  = NULL
-  };
-  
-  //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
-  if (httpd_start(&stream_httpd, &http_config) == ESP_OK) {
-    httpd_register_uri_handler(stream_httpd, &index_uri);
+      xSemaphoreGive(cameraMutex);
+    }
   }
 }
 
@@ -412,5 +390,12 @@ void app_main(void)
     }
 
     ESP_ERROR_CHECK(init_camera());
-    startCameraServer();
+    
+    //create mutex for camera data
+    cameraMutex = xSemaphoreCreateMutex();
+    httpReqQueue = xQueueCreate( 5, sizeof( httpd_req_t* ) );
+    cameraTaskEventGroup = xEventGroupCreate();
+
+    xTaskCreate(CameraCapture_task, "CameraCapture_task", 4096, NULL, 4, NULL);
+    xTaskCreate(SaveCamera_task, "SaveCamera_task", 4096, NULL, 5, NULL);
 }
